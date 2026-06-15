d7ff7efcabd69cce906eb2b534dcf5c79079919c
jeroenhuijben <jeroenhuijben@icloud.com>
2026-06-15 09:24:52 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.KVW_SUPABASE_CONFIG = {
-  url: "https://vdyfptpjnfigfvfvjbux.supabase.co/rest/v1/",
+  url: "https://vdyfptpjnfigfvfvjbux.supabase.co",
   anonKey: "sb_publishable_Tw_DtZGVv_YGDlDmrB2_Vw_OVf1AlAc"
 };
